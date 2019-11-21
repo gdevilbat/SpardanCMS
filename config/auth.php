c5ae312d7be5a23846ea95732b183b199abfac49
unknown <maldinuribrahim@gmail.com>
2019-11-21 14:30:27 +0700

    Use Login Model From Vendor

--- config/auth.php
+++ config/auth.php
@@ -68,7 +68,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\User::class,
+            'model' => \Gdevilbat\SpardaCMS\Modules\Core\Entities\User::class,
         ],
 
         // 'users' => [
